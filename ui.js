c496342269b7e001c696884afadff20d7c7c93d2
kaylamoon0000 <kaylamoon0000@gmail.com>
2026-03-25 14:11:43 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,3 +1,13 @@
+const cellTypeSelect = document.getElementById("cellType");
+
+// Populate dropdown dynamically from CELL_TYPES
+for (const key in CELL_TYPES) {
+  const option = document.createElement("option");
+  option.value = key;
+  option.textContent = CELL_TYPES[key].name;
+  cellTypeSelect.appendChild(option);
+}
+
 document.getElementById("playBtn").onclick = () => {
   running = !running;
   document.getElementById("playBtn").textContent = running
